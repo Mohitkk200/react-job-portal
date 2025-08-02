afc5a00e7d5ffa0776f12182956c025a43d150a4
Your Name <your@email.com>
2025-08-02 13:24:24 +0530

    added root route to fix Cannot GET /

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,8 @@ cloudinary.v2.config({
 app.listen(process.env.PORT, () => {
   console.log(`Server running at port ${process.env.PORT}`);
 });
+
+app.get('/', (req, res) => {
+  res.send('Server is working 👌');
+});
+
